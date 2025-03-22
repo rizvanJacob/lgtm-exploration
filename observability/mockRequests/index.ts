64bd93b75ca3a97d008bdf01fda2afc598388e24
R Jacob <rizzy@live.com.sg>
2025-03-22 22:18:05 +0800

    fix mock request script

--- observability/mockRequests/index.ts
+++ observability/mockRequests/index.ts
@@ -1,13 +1,20 @@
-import axios, { Method } from 'axios';
+import axios from "axios";
 
-const host = "spring-micrometer-app"
-const port = "8080"
+const host = "spring-micrometer-app";
+const port = "8080";
+
+const axiosInstance = axios.create({
+  baseURL: `http://${host}:${port}`,
+});
 
 // Define your requests
-const requests: { url: string; method: Method }[] = [
-  { endpoint: '/wait/0', method: 'GET' },
-  { endpoint: '/wait/1', method: 'GET' },
-  { endpoint: '/wait/2', method: 'GET' },
+const requests: {
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "DELETE";
+}[] = [
+  { endpoint: "/wait/0", method: "GET" },
+  { endpoint: "/wait/1", method: "GET" },
+  { endpoint: "/wait/2", method: "GET" },
 ];
 
 // Function to select a random request
@@ -18,18 +25,26 @@ function getRandomRequest() {
 
 // Function to perform the request
 async function performRequest() {
-    //randomly skip requests
-    if (Math.random() < 0.5) {
-        return;
-        }
+  //randomly skip requests
+  if (Math.random() < 0.5) {
+    return;
+  }
 
-  const { url, method } = getRandomRequest();
+  const { endpoint, method } = getRandomRequest();
+  console.log(`Executing ${method} to ${endpoint}`);
 
-    axios({
+  axiosInstance
+    .request({
       method,
-      url,
+      url: endpoint,
       // Provide dummy data for POST/PUT
-      data: method === 'POST' || method === 'PUT' ? { title: 'foo', body: 'bar', userId: 1 } : undefined,
+      data:
+        method === "POST" || method === "PUT"
+          ? { title: "foo", body: "bar", userId: 1 }
+          : undefined,
+    })
+    .catch((_exception) => {
+      console.log(`${method} to ${endpoint} failed`);
     });
 }
 
