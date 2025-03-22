0ebe0298c9231ebd0e9b177883eb17fbc0a78423
R Jacob <rizzy@live.com.sg>
2025-03-22 20:16:35 +0800

    observability works

--- src/main/java/com/example/learning/WebController.java
+++ src/main/java/com/example/learning/WebController.java
@@ -2,22 +2,21 @@ package com.example.learning;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-    private final int maxWaitDurationSeconds = 10;
+    private static final int maxWaitDurationSeconds = 10;
 
-    @GetMapping("/wait/:waitDurationSeconds")
-    public ResponseEntity<String> simulateRequest(@RequestParam int waitDurationSeconds) {
+    @GetMapping("/wait/{waitDurationSeconds}")
+    public ResponseEntity<String> simulateRequest(@PathVariable int waitDurationSeconds) {
         var waitDurationMillis = Math.min(waitDurationSeconds, maxWaitDurationSeconds) * 1000;
         try {
             Thread.sleep(waitDurationMillis);
             return ResponseEntity.ok(String.format("Completed request in %d seconds", waitDurationMillis / 1000));
         } catch (InterruptedException e) {
-            return ResponseEntity.badRequest()
-                    .body("Could not complete request!");
+            return ResponseEntity.badRequest().body("Could not complete request!");
         }
     }
 }
