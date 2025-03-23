21ec45ff360eaf49296cca091973dd203e97cdd4
R Jacob <rizzy@live.com.sg>
2025-03-23 15:26:34 +0800

    add db and simulate db read/writes

--- src/main/java/com/example/learning/MockEntityController.java
+++ src/main/java/com/example/learning/MockEntityController.java
@@ -1,20 +1,25 @@
 package com.example.learning;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-public class CrudSimulator {
+@Component
+public class MockEntityController {
     private final Random random = new Random();
     private final MockEntityService service;
-    public CrudSimulator(MockEntityService service, ScheduledExecutorService executor) {
+
+    public MockEntityController(MockEntityService service, ScheduledExecutorService executor) {
         this.service = service;
         executor.scheduleAtFixedRate(this::simulateCrud, 0, 100, TimeUnit.MILLISECONDS);
     }
-    public void simulateCrud(){
+
+    public void simulateCrud() {
         var entities = service.findAll();
         if (entities.isEmpty()) {
             service.createNew("First Entity");
@@ -23,11 +28,15 @@ public class CrudSimulator {
         int option = random.nextInt(4);
         long idToGet = entities.get(random.nextInt(entities.size()))
                 .getId();
-        switch (option) {
-            case 0 -> service.createNew("New Entity");
-            case 1 -> service.updateById(idToGet, "Updated Entity");
-            case 2 -> service.findById(idToGet);
-            case 3 -> service.deleteById(idToGet);
+        try {
+            switch (option) {
+                case 0 -> service.createNew("New Entity");
+                case 1 -> service.updateById(idToGet, "Updated Entity");
+                case 2 -> service.findById(idToGet);
+                case 3 -> service.deleteById(idToGet);
+            }
+        } catch (NoSuchElementException ex) {
+            log.error("Failed to perform CRUD operation ", ex);
         }
     }
 }
--- src/main/java/com/example/learning/MockEntityService.java
+++ src/main/java/com/example/learning/MockEntityService.java
@@ -2,29 +2,38 @@ package com.example.learning;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Service
 @RequiredArgsConstructor
+@Slf4j
 public class MockEntityService {
     private final MockEntityRepository repository;
     @Transactional
     public MockEntity createNew(String text){
+        log.info("Simulating create entity: {}", text);
         var entity = new MockEntity();
         entity.setText(text);
         return repository.save(entity);
     }
     @Transactional
     public MockEntity findById(Long id){
+        log.info("Simulating find entity by id ({})", id);
         return repository.findById(id)
                 .orElseThrow();
     }
     @Transactional
     public List<MockEntity> findAll(){
+        log.info("Simulating find all entities");
         return repository.findAll();
     }
     @Transactional
     public MockEntity updateById(Long id, String text){
+        log.info("Simulating update entity by id ({}): {}", id, text);
         var entity = repository.findById(id)
                 .orElseThrow();
         entity.setText(text);
@@ -32,6 +41,7 @@ public class MockEntityService {
     }
     @Transactional
     public void deleteById(Long id){
+        log.info("Simulating delete entity by id ({})", id);
         repository.deleteById(id);
     }
 }
